cfa5d98bdeb06135e1c53e305024057894457263
Benyo <b.farkasbence@gmail.com>
2019-03-25 13:39:11 +0100

    pep8 changes in server.py

--- server.py
+++ server.py
@@ -1,12 +1,12 @@
 from flask import Flask, render_template
 
-app= Flask(__name__)
+app = Flask(__name__)
+
 
 @app.route('/')
 @app.route("/list")
-def list():
+def list_of_questions():
     return render_template("list.html")
-
 
 
 if __name__ == "__main__":
