e8afda749fa28ee7150c7e9cf1d15fd1815f562e
leinwanderIstvan <coolcodebasic@gmail.com>
2019-04-17 11:14:46 +0200

    New_comment query add to data_manager, new route add in server.py

--- data_manager.py
+++ data_manager.py
@@ -164,5 +164,5 @@ def complement_new_comment_of_question(cursor, message, question_id):
     submission_time = get_submission_time()
     cursor.execute("""
                     INSERT INTO comment ("question_id", "answer_id" , "message" ,"submission_time", "edited_count")
-                    VALUES (%(question_id)s, None, %(message)s, (%(submission_time)s, None); """,
+                    VALUES (%(question_id)s, None, %(message)s, %(submission_time)s, None); """,
                    {"question_id": question_id,  "submission_time": submission_time,"message": message })
